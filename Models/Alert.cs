ea5b3921df227cd75b8dd26d75b68f6bfbfe010f
rafael1209 <mr.chasmanrafael@gmail.com>
2024-10-19 23:19:00 +0300

    Add project files.

---
+++ Models/Alert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsraelHomeFrontCommandAPI.Models
+{
+    public class Alert
+    {
+        public string Id { get; set; }
+        public string Cat { get; set; }
+        public string Title { get; set; }
+        public List<string> Data { get; set; }
+        public string Desc { get; set; }
+    }
+}
