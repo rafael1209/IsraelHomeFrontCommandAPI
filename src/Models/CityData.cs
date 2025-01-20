1e6110f9bb6d0b4459e38721ac23f8728ec6e43e
chasman <chasman@B203-52-129>
2025-01-20 16:09:03 +0200

    fix

---
+++ src/Models/CityData.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IsraelHomeFrontCommandAPI.Models
+{
+    public class CityData
+    {
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("areaid")]
+        public int AreaId { get; set; }
+
+        [JsonPropertyName("areaname")]
+        public string AreaName { get; set; }
+
+        [JsonPropertyName("label_he")]
+        public string LabelHe { get; set; }
+
+        [JsonPropertyName("migun_time")]
+        public int MigunTime { get; set; }
+    }
+}
