cdc3f794bf66f8a73437427243b13f089d06317c
rafael1209 <mr.chasmanrafael@gmail.com>
2025-06-14 21:50:16 +0300

    Enhance alert response models and update tests
    - Added `Cities` property to `ActiveAlertsResponse`.
    - Marked `Data` as required in `AlertData`.
    - Included nullable `alerts` list in `AlertHistoryApiResponse`.
    - Added `AlertType` and `City` properties in `AlertsHistoryResponse`.
    - Modified namespace and introduced `Test` class in `Test.cs`.
    - Updated `TestCurrentAlert` and `TestAlertsHistory` methods with `[Fact]` attributes and improved formatting.

--- tests/IsraelHomeFrontCommandAPI.IntegrationTests/Test.cs
+++ tests/IsraelHomeFrontCommandAPI.IntegrationTests/Test.cs
@@ -1,28 +1,27 @@
 ﻿using IsraelHomeFrontCommandAPI.Enums;
 using Xunit;
 
-namespace IsraelHomeFrontCommandAPI.IntegrationTests
+namespace IsraelHomeFrontCommandAPI.IntegrationTests;
+
+public class Test
 {
-    public class Test
+    [Fact]
+    public async Task TestCurrentAlert()
     {
-        [Fact]
-        public async Task TestCurrentAlert()
-        {
-            HomeFrontCommandClient client = new HomeFrontCommandClient();
+        HomeFrontCommandClient client = new HomeFrontCommandClient();
 
-            var response = await client.GetActiveAlertsAsync();
+        var response = await client.GetActiveAlertsAsync();
 
-            Assert.True(response != null);
-        }
+        Assert.True(response != null);
+    }
 
-        [Fact]
-        public async Task TestAlertsHistory()
-        {
-            HomeFrontCommandClient client = new HomeFrontCommandClient(Language.Russian);
+    [Fact]
+    public async Task TestAlertsHistory()
+    {
+        HomeFrontCommandClient client = new HomeFrontCommandClient(Language.Russian);
 
-            var response = await client.GetAlertsHistoryLastDayAsync();
+        var response = await client.GetAlertsHistoryLastDayAsync();
 
-            Assert.True(response != null);
-        }
+        Assert.True(response != null);
     }
 }
